6fc3a99205795565c46366423b7e56a39cff46fe
Vagelis Xaniadakis <echaniadakis@gmail.com>
2022-01-10 12:24:11 +0200

    commit

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import pandas
 class ArgumentParser():
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('-d', type=str)
+        parser.add_argument('-d', type=str, default="Examples/nasd_input.csv")
         parser.add_argument('-n', type=int, default=10)
-        parser.add_argument('-q', type=str)
+        parser.add_argument('-q', type=str, default="Examples/nasd_query.csv")
         parser.add_argument('-od', type=str, default="output_dataset.csv")
         parser.add_argument('-oq', type=str, default="output_query_file.csv")
         parser.add_argument('-mae', type=float, default=0.65)
